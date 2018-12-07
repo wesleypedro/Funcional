a885221b8ecbf585b41ea4fcd40e55b77ee359c1
Wesley Pedro <40727770+wesleypedro@users.noreply.github.com>
2018-12-07 12:17:48 -0300

    Remoção de partes desnecessárias

--- simetrica.py
+++ simetrica.py
@@ -1,5 +1,5 @@
 pertence = lambda x, l : (len(l) > 0 and (l[0] == x or pertence(x, l[1:])))
 diferenca = lambda a, b : (len(a) > 0 and ((not pertence(a[0], b) and [a[0]] + diferenca(a[1:], b)) or (diferenca(a[1:], b)))) or []
-uniao = lambda a, b : (len(a) == 0 and b) or (len(b) == 0 and a) or (a+b)
+uniao = lambda a, b : a+b
 simetrica = lambda a, b : uniao(diferenca(a,b), diferenca(b,a))
 print(simetrica(map(int, raw_input().split()), map(int, raw_input().split())))
